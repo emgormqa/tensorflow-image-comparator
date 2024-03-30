7968188ad25652b0619f7fdae3877fe7bffa785a
emgormqa <151537293+emgormqa@users.noreply.github.com>
2024-03-30 16:16:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
+# load model
 model = load_model('x_classifier.h5')
 
+# image to classify load function
 def load_and_process_image(image_path):
     img = cv2.imread(image_path)
     img = cv2.resize(img, (512, 512))                                          
@@ -11,6 +13,7 @@ def load_and_process_image(image_path):
     img = np.expand_dims(img, axis=0)      
     return img
 
+# classifyer funcftion
 def classify_image(image_path):
     img = load_and_process_image(image_path)
     prediction = model.predict(img)
@@ -20,7 +23,9 @@ def classify_image(image_path):
     else:
         return "2"
 
+# image to classify
 image_path = "test.jpg"
 
+# print result
 result = classify_image(image_path)
 print(result)
