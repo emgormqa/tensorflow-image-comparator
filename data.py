258253ce64c075adb17b1fe3e844b9c82beda1e1
emgormqa <151537293+emgormqa@users.noreply.github.com>
2024-03-30 16:12:58 +0100

    Update data.py

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# load and resize images function
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
@@ -12,16 +13,21 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
+# load images from 2 folders
 1_images = load_images_from_folder("1")
 2_images = load_images_from_folder("2")
 
+# tagging
 labels = [1] * len(1_images) + [0] * len(2_images)
 
+# creating array from all images
 images = np.array(1_images + 2_images)
 labels = np.array(labels)
 
+# separation of data into training and test data
 train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2, random_state=42)
 
+# save all data
 np.save('train_images.npy', train_images)
 np.save('train_labels.npy', train_labels)
 np.save('test_images.npy', test_images)
